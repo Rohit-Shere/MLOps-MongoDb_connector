6390e57835c7e75f738ba5e2e49166292bef0730
Rohit <rhtshere@gmail.com>
2024-03-14 22:11:11 +0530

    a

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ with open('README.md', 'r', encoding='utf-8') as f:
    
 
 __version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
+REPO_NAME = "MLOps-MongoDb_connector
+"
 PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+AUTHOR_USER_NAME = "Rohit-Shere"
+AUTHOR_EMAIL = "rhtshere@gmail.com"
 
 setup(
     name=PKG_NAME,
